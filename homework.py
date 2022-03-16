5529fedd61dd0a33a5209b42aaeced17686d3b1c
Антон Михайлов <insept17@yandex.ru>
2022-03-16 18:03:51 +0600

    1

--- homework.py
+++ homework.py
@@ -1,11 +1,18 @@
-...
+import logging
+import os
+import sys
+import time
+from http import HTTPStatus
+
+import requests
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,66 +25,91 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)s, %(levelname)s, %(message)s',
+)
+
 
 def send_message(bot, message):
-    ...
+    logger.info(f'Отправка сообщения: {message}')
+    bot_message = bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    if not bot_message:
+        raise telegram.TelegramError(f'Ошибка при отправке: {message}')
+    else:
+        logger.info(f'Отправка сообщения: {message}')
 
 
 def get_api_answer(current_timestamp):
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
-
-    ...
+    '''Делает запрос к эндпоинту API-сервиса.'''
+    params = {'from_date': current_timestamp}
+    try:
+        api_answer = requests.get(ENDPOINT, headers=HEADERS, params=params)
+        logging.info('Сервер работает')
+        if api_answer.status_code != HTTPStatus.OK:
+            raise Exception('Ошибка в коде состояния HTTP')
+        return api_answer.json()
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        raise error
 
 
 def check_response(response):
-
-    ...
+    '''Проверяет ответ API на корректность.'''
+    if not isinstance(response['homeworks'], list):
+        logger.error('Не найден ключ homeworks')
+        raise AssertionError('Ключи отсутствуют')
+    elif 'current_date' not in response:
+        logger.error('Не найден ключ current_date')
+        raise AssertionError('Ключи отсутствуют')
+    else:
+        logger.info('Проверка корректности API пройдена')
+    return response.get('homeworks')
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    '''Извлекает информацию о конкретной
+     домашней работе статус этой работы.'''
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status not in HOMEWORK_STATUSES.keys():
+        logger.error('Такого статуса нет')
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    '''Возвращает переменные окружения если они доступны.'''
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
+    if not check_tokens():
+        logger.critical('Отсутствуют переменные окружения!')
+        raise SystemExit()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
-
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            if isinstance(homework, list) and homework:
+                send_message(bot, parse_status(homework[0]))
+            else:
+                logger.debug('Статус задания не обновлён')
             time.sleep(RETRY_TIME)
-
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error(message)
+            send_message(bot, message)
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as error:
+        logger.error(f'Непредвиденная ошибка: {error}')
