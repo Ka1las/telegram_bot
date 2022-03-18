6dda26e328cbf687ceeeb0946a09fd1d45673b92
Антон Михайлов <insept17@yandex.ru>
2022-03-18 20:33:22 +0600

    2

--- homework.py
+++ homework.py
@@ -22,13 +22,16 @@ logger = logging.getLogger(__name__)
 
 
 class NoKeyError(Exception):
+    """Наследую исключение для конкретной задачи."""
     def __init__(self, *args):
+        """Вызывается при создании экземпляра."""
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Вызывается при выводе экземпляра на экран."""
         if self.message:
             return 'NoKeyError, {0} '.format(self.message)
         else:
@@ -36,13 +39,16 @@ class NoKeyError(Exception):
 
 
 class NoConnectionError(Exception):
+    """Наследую исключение для конкретной задачи."""
     def __init__(self, *args):
+        """Вызывается при создании экземпляра."""
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Вызывается при выводе экземпляра на экран."""
         if self.message:
             return 'NoConnection {0} '.format(self.message)
         else:
